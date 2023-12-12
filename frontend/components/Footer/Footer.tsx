8ca34fbe6d4e8dac9aacd344d85944d34135fa49
jeli-t <jelitotomek@gmail.com>
2023-12-12 14:54:25 +0100

    add social icons in Footer

--- frontend/components/Footer/Footer.tsx
+++ frontend/components/Footer/Footer.tsx
@@ -1,8 +1,7 @@
-import { Group, Container } from '@mantine/core';
+import { Group, Container, ActionIcon, rem } from '@mantine/core';
 import classes from './Footer.module.css';
 import {useTranslations} from 'next-intl';
-import { ColorSchemeToggle } from '../ColorSchemeToggle/ColorSchemeToggle';
-import { LocaleSwitcher } from '../LanguagePicker/LocaleSwitcher';
+import { IconBrandInstagram, IconMail, IconBrandFacebook } from '@tabler/icons-react';
 import {Link} from '../../navigation';
 import { Logo } from './Logo'
 
@@ -13,18 +12,23 @@ export function Footer() {
   return (
       <Container fluid className={classes.wrapper}>
         <div className={classes.inner}>
-          <div className={classes.logo_wrapper}>
-            <Logo></Logo>
-          </div>
+          <Logo></Logo>
           <Group gap={5} visibleFrom="sm">
             <p><Link href='/' key={t('home')} className={classes.link}>{t('home')}</Link></p>
             <p><Link href='/portfolio' key={t('portfolio')} className={classes.link}>{t('portfolio')}</Link></p>
             <p><Link href='/about' key={t('about')} className={classes.link}>{t('about')}</Link></p>
             <p><Link href='/contact' key={t('contact')} className={classes.link}>{t('contact')}</Link></p>
           </Group>
-          <Group className={classes.socials_wrapper}>
-            <p>IG</p>
-            <p>FB</p>
+          <Group>
+            <ActionIcon size="md" variant="default" radius="xl">
+              <IconMail style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            </ActionIcon>
+            <ActionIcon size="md" variant="default" radius="xl">
+              <IconBrandFacebook style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            </ActionIcon>
+            <ActionIcon size="md" variant="default" radius="xl">
+              <IconBrandInstagram style={{ width: rem(18), height: rem(18) }} stroke={1.5} />
+            </ActionIcon>
           </Group>
         </div>
       </Container>
