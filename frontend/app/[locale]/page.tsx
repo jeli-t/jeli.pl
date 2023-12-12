5a2f1c87241830772b581dbf511e48b8c66e76e9
jeli-t <jelitotomek@gmail.com>
2023-12-12 14:09:27 +0100

    add ConstructionSite

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,29 +1,15 @@
 import classes from './page.module.css'
-import {Link} from '../../navigation';
 import {useTranslations} from 'next-intl';
-import { ColorSchemeToggle } from '../../components/ColorSchemeToggle/ColorSchemeToggle';
-import { Button } from '@mantine/core';
+import { ConstructionSite } from '../../components/ConstructionSite/ConstructionSite';
+import { Container } from '@mantine/core';
 
 
 export default function Home() {
   const t = useTranslations('HomePage');
 
   return (
-    <div>
-      <br></br>
-      <h1>{t('title')}</h1>
-      <h2><Link href="/portfolio">{t('portfolio')}</Link></h2>
-      <h2><Link href="/about">{t('about')}</Link></h2>
-      <h2><Link href="/contact">{t('contact')}</Link></h2>
-      <br></br>
-      <h2><Link href="/" locale='pl'>Polish</Link></h2>
-      <h2><Link href="/" locale='en'>English</Link></h2>
-      <br></br>
-      <ColorSchemeToggle />
-      <br></br>
-      <Button variant='filled' m={'sm'}>Theme check</Button>
-      <br></br>
-      <Button variant='filled' m={'sm'} className={classes.button}>Theme check</Button>
-    </div>
+    <Container fluid>
+      <ConstructionSite></ConstructionSite>
+    </Container>
   )
 }
---
+++ frontend/components/ConstructionSite/ConstructionSite.tsx
@@ -0,0 +1,24 @@
+import { Button, Container, Space, Title } from '@mantine/core';
+import classes from './ConstructionSite.module.css';
+import {useTranslations} from 'next-intl';
+import Image from 'next/image'
+import construction from './construction.svg';
+import {Link} from '../../navigation';
+
+
+export function ConstructionSite() {
+  const t = useTranslations('ConstructionSite');
+
+  return (
+    <Container fluid className={classes.wrapper}>
+        <Image src={construction} alt="Construction site" title='Construction site' loading='eager' height={400} />
+        <Title order={2}>
+            {t('message')}
+        </Title>
+        <Space h={'xl'}></Space>
+        <Link href={'/'}>
+        <Button size='sm' className={classes.button}>{t('button')}</Button>
+        </Link>
+    </Container>
+  );
+}
