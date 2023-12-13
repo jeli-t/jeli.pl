6782fada53fadf9110187dfa064085c5aef1474f
jeli-t <jelitotomek@gmail.com>
2023-12-13 18:46:31 +0100

    redirect to custom 404 page if page not found

---
+++ frontend/app/[locale]/not-found.tsx
@@ -0,0 +1,12 @@
+import { PageNotFound } from '../../components/PageNotFound/PageNotFound';
+import { Container } from '@mantine/core';
+
+
+export default function notFound() {
+
+  return (
+    <Container fluid>
+      <PageNotFound></PageNotFound>
+    </Container>
+  )
+}
