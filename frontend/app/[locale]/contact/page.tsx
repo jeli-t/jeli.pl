fca4d5bdb0e65808f1da504edcd1f7988c19245f
jeli-t <jelitotomek@gmail.com>
2023-12-13 21:12:54 +0100

    add ContactSection

--- frontend/app/[locale]/contact/page.tsx
+++ frontend/app/[locale]/contact/page.tsx
@@ -1,12 +1,12 @@
 import styles from './page.module.css'
-import { ConstructionSite } from '../../../components/ConstructionSite/ConstructionSite';
+import { ContactSection } from '@/components/ContactSection/ContactSection';
 import { Container } from '@mantine/core';
 
 
 export default function Contact() {
   return (
     <Container fluid>
-      <ConstructionSite></ConstructionSite>
+      <ContactSection></ContactSection>
     </Container>
   )
 }
