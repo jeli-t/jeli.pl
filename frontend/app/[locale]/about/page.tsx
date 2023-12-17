82ec90fb152456e724984c41cb992eb303c1ca0c
jeli-t <jelitotomek@gmail.com>
2023-12-17 18:19:01 +0100

    add MyCompetence

--- frontend/app/[locale]/about/page.tsx
+++ frontend/app/[locale]/about/page.tsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css'
 import { AboutMeSection } from '@/components/AboutMeSection/AboutMeSection';
+import { MyCompetence } from '@/components/MyCompetence/MyCompetence';
 import { Container } from '@mantine/core';
 
 
 export default function About() {
   return (
-    <Container fluid>
+    <Container fluid p={0}>
       <AboutMeSection></AboutMeSection>
+      <MyCompetence></MyCompetence>
     </Container>
   )
 }
---
+++ frontend/components/MyCompetence/MyCompetence.tsx
@@ -0,0 +1,61 @@
+import { Button, Container, Group, Space, Text, Title } from '@mantine/core';
+import classes from './MyCompetence.module.css';
+import {useTranslations} from 'next-intl';
+
+
+export function MyCompetence() {
+  const t = useTranslations('MyCompetence');
+
+  return (
+    <Container fluid className={classes.component}>
+      <Container fluid className={classes.wrapper}>
+        <div className={classes.left_section}>
+          <Title order={1}>
+            {t('title')}
+          </Title>
+          <Space my="sm" />
+          <Text size="lg">
+            {t('content1')}
+          </Text>
+          <Space my="sm" />
+          <Text size="lg">
+            {t('content2')}
+          </Text>
+          <Space my="sm" />
+          <Text size="lg">
+            {t('content3')}
+          </Text>
+        </div>
+        <div className={classes.right_section}>
+          <Title order={1}>
+            {t('skills')}
+          </Title>
+          <Space my="sm" />
+          <Group>
+          <Button variant='light' className={classes.skill}>SEO</Button>
+            <Button variant='light' className={classes.skill}>Node</Button>
+            <Button variant='light' className={classes.skill}>React</Button>
+            <Button variant='light' className={classes.skill}>Mantine</Button>
+            <Button variant='light' className={classes.skill}>Responsive Design</Button>
+            <Button variant='light' className={classes.skill}>Django</Button>
+            <Button variant='light' className={classes.skill}>Git</Button>
+            <Button variant='light' className={classes.skill}>Github</Button>
+            <Button variant='light' className={classes.skill}>JavaScript</Button>
+            <Button variant='light' className={classes.skill}>TypeScript</Button>
+            <Button variant='light' className={classes.skill}>PostgreSQL</Button>
+            <Button variant='light' className={classes.skill}>MySQL</Button>
+            <Button variant='light' className={classes.skill}>HTML</Button>
+            <Button variant='light' className={classes.skill}>CSS</Button>
+            <Button variant='light' className={classes.skill}>PHP</Button>
+            <Button variant='light' className={classes.skill}>Java</Button>
+            <Button variant='light' className={classes.skill}>Python</Button>
+            <Button variant='light' className={classes.skill}>C++</Button>
+            <Button variant='light' className={classes.skill}>Linux</Button>
+            <Button variant='light' className={classes.skill}>DevOps</Button>
+            <Button variant='light' className={classes.skill}>GCP</Button>
+          </Group>
+        </div>
+      </Container>
+    </Container>
+  );
+}
