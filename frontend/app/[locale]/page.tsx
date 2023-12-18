8d0e74da5afe973cc1d04598e3fbeaaeda84bbc4
jeli-t <jelitotomek@gmail.com>
2023-12-18 22:59:09 +0100

    add HeroSection

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -1,11 +1,9 @@
 import classes from './page.module.css'
-import {useTranslations} from 'next-intl';
 import { HeroSection } from '@/components/HeroSection/HeroSection';
 import { Container } from '@mantine/core';
 
 
 export default function Home() {
-  const t = useTranslations('HomePage');
 
   return (
     <Container fluid>
--- frontend/components/HeroSection/HeroSection.tsx
+++ frontend/components/HeroSection/HeroSection.tsx
@@ -1,15 +1,36 @@
 import classes from './HeroSection.module.css';
 import {useTranslations} from 'next-intl';
-import { Logo } from './Logo'
-import { Container } from '@mantine/core';
+import { Title, Text, Button, Container } from '@mantine/core';
+import { Dots } from './Dots';
 
 
 export function HeroSection() {
-  const t = useTranslations('HeaderMenu');
+  const t = useTranslations('HeroSection');
 
   return (
-    <Container fluid className={classes.wrapper}>
-      <Logo></Logo>
+    <Container fluid className={classes.component}>
+      <Container className={classes.wrapper} size={1400}>
+        <Dots className={classes.dots} style={{ left: 0, top: 0 }} />
+        <Dots className={classes.dots} style={{ left: 60, top: 0 }} />
+        <Dots className={classes.dots} style={{ left: 0, top: 140 }} />
+        <Dots className={classes.dots} style={{ right: 0, top: 60 }} />
+
+        <div className={classes.inner}>
+          <Title className={classes.title}>
+            <Text component="span" className={classes.highlight} inherit>
+              Tomasz Jelito
+            </Text>
+          </Title>
+          <Title className={classes.title}>
+            full-stack web developer
+          </Title>
+          <Container p={0} size={600}>
+            <Text size="lg" c="dimmed" className={classes.description}>
+              {t('content')}
+            </Text>
+          </Container>
+        </div>
+      </Container>
     </Container>
   );
 }
