972f6e181503b738756839bd3fd2ae6670a1a601
jeli-t <jelitotomek@gmail.com>
2023-12-31 18:09:21 +0100

    add WhatIOffer section (no content)

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -2,6 +2,7 @@ import classes from './page.module.css'
 import { Container } from '@mantine/core';
 import { HeroSection } from '@/components/HeroSection/HeroSection';
 import { AboutPreview } from '@/components/AboutPreview/AboutPreview';
+import { WhatIOffer } from '@/components/WhatIOffer/WhatIOffer';
 
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
   return (
     <Container fluid p={0}>
       <HeroSection></HeroSection>
+      <WhatIOffer></WhatIOffer>
       <AboutPreview></AboutPreview>
     </Container>
   )
