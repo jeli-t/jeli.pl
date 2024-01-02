7a37726073c2b709fcdaeaa661994a6b73ce00c5
jeli-t <jelitotomek@gmail.com>
2024-01-02 15:53:46 +0100

    add CallToAction on Portfolio page

--- frontend/app/[locale]/portfolio/page.tsx
+++ frontend/app/[locale]/portfolio/page.tsx
@@ -1,12 +1,14 @@
 import styles from './page.module.css'
 import { Container } from '@mantine/core';
 import { PortfolioProjects } from '@/components/PortfolioProjects/PortfolioProjects';
+import { CallToAction } from '@/components/CallToAction/CallToAction';
 
 
 export default function Portfolio() {
   return (
     <Container fluid p={0} pt={100}>
       <PortfolioProjects></PortfolioProjects>
+      <CallToAction></CallToAction>
     </Container>
   )
 }
