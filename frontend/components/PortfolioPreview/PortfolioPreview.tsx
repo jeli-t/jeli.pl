8dcc44e6f48ef7ad3c5f64e551ef98f2c83aaa69
jeli-t <jelitotomek@gmail.com>
2024-03-27 20:14:27 +0100

    redesign PortfolioPreview

--- frontend/components/PortfolioPreview/PortfolioPreview.tsx
+++ frontend/components/PortfolioPreview/PortfolioPreview.tsx
@@ -1,38 +1,138 @@
-import { Button, Container, Space, Stack, Text, Title } from '@mantine/core';
+import { Button, Container, Space, Text, Title, Card, Group, Flex, SimpleGrid, Badge } from '@mantine/core';
+import { IconWorldWww, IconBrandGithub } from '@tabler/icons-react';
 import classes from './PortfolioPreview.module.css';
 import {useTranslations} from 'next-intl';
-import Image from 'next/image'
-import portfolio_preview from './portfolio_preview.png';
 import {Link} from '@/navigation';
+
+
+interface ProjectProps {
+  image: string;
+  title: string;
+  page_button: string;
+  page_link: string;
+  source_button: string;
+  source_link: string;
+}
+
+const mockdata = [
+  {
+    image: "url(/img/agency.png)",
+    title: 'agency_title',
+    description: 'agency_description',
+    page_button: 'live_website',
+    page_link: 'https://jeli-agency.vercel.app/',
+    source_button: 'source_code',
+    source_link: 'https://github.com/jeli-t/agency'
+  },
+  {
+    image: "url(/img/portfolio.png)",
+    title: 'portfolio_title',
+    description: 'portfolio_description',
+    page_button: '',
+    page_link: '',
+    source_button: 'source_code',
+    source_link: 'https://github.com/jeli-t/jeli.pl'
+  },
+  {
+    image: "url(/img/crisis_corridor.png)",
+    title: 'crisis_coriddor_title',
+    description: 'crisis_coriddor_description',
+    page_button: '',
+    page_link: '',
+    source_button: 'source_code',
+    source_link: 'https://github.com/jeli-t/zegluga-game-jam-2023'
+  },
+];
+
+
+export function ImageCard({image, title, page_button, page_link, source_button, source_link}: ProjectProps) {
+  const t = useTranslations('PortfolioProjects');
+
+  return (
+    <Card
+      p="lg"
+      shadow="lg"
+      className={classes.card}
+      radius="md"
+      component="a"
+      href={page_link === "" ? source_link : page_link}
+      target="_blank"
+    >
+      <div
+        className={classes.image}
+        style={{backgroundImage: image}}
+      />
+      <div className={classes.overlay} />
+
+      <div className={classes.content}>
+        <div>
+          <Text size="xl" className={classes.title} fw={500}>
+            {t(title)}
+          </Text>
+
+          <Flex
+            mih={50}
+            gap="sm"
+            justify="flex-start"
+            align="center"
+            direction="row"
+            wrap="wrap"
+          >
+            {page_button !== '' && (
+              <a href={page_link} target='_blank' className={classes.link}>
+                <Group gap="sm">
+                  <IconWorldWww stroke={1.5} />
+                  <Text size="lg" >
+                    {t(page_button)}
+                  </Text>
+                </Group>
+              </a>
+            )}
+            {source_button !== '' && (
+              <a href={source_link} target='_blank' className={classes.link}>
+                <Group gap="sm">
+                  <IconBrandGithub stroke={2} />
+                  <Text size="lg">
+                    {t(source_button)}
+                  </Text>
+                </Group>
+              </a>
+            )}
+          </Flex>
+        </div>
+      </div>
+    </Card>
+  );
+}
 
 
 export function PortfolioPreview() {
   const t = useTranslations('PortfolioPreview');
+  const items = mockdata.map((item) => <ImageCard {...item} key={item.title} />);
 
   return (
     <Container fluid className={classes.component}>
       <Container fluid className={classes.wrapper}>
-        <div className={classes.left_section}>
-          <Image src={portfolio_preview} alt={t('img')} title={t('img')} loading='eager' height={500} />
-        </div>
-        <div className={classes.right_section}>
-          <Title order={1} className={classes.title}>
-            {t('title')}
-          </Title>
-          <Space my="sm" />
-          <Text className={classes.description}>
-            {t('description')}
-          </Text>
-          <Space my="sm" />
+        <Group justify="center">
+          <Badge variant="filled" size="lg">
+            Portfolio
+          </Badge>
+        </Group>
+        <Title order={1} mt={"sm"} className={classes.section_title}>
+          {t('title')}
+        </Title>
+        <SimpleGrid cols={{base: 1, sm: 3}} spacing="md" mt={50}>
+          {items}
+        </SimpleGrid>
+        <Group justify="center">
           <div>
-            <Link href={'/portfolio'}>
-              <Button className={classes.link}>Portfolio</Button>
+            <Link href="/portfolio">
+              <Button variant="outline" size="md" mt={"xl"}>
+                {t('button')}
+              </Button>
             </Link>
-            <a href="https://github.com/jeli-t">
-              <Button className={classes.link}>Github</Button>
-            </a>
           </div>
-        </div>
+        </Group>
       </Container>
     </Container>
   );
