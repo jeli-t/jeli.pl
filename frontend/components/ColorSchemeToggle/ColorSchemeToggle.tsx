f691c3fa051258d323d166515c3b6ede4ee978f9
jeli-t <jelitotomek@gmail.com>
2024-03-29 10:20:17 +0100

    fix hydration error for ColorSchemeToggle

--- frontend/components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ frontend/components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,23 +1,42 @@
 'use client';
 
-import { Group, Switch, useMantineColorScheme, useMantineTheme } from '@mantine/core';
+import { Switch, useMantineColorScheme, useMantineTheme } from '@mantine/core';
 import { IconMoonStars, IconSun } from '@tabler/icons-react';
 import classes from './ColorSchemeToggle.module.css';
+import { useState, useEffect } from 'react'
 
 
 export function ColorSchemeToggle() {
-  const { colorScheme, toggleColorScheme } = useMantineColorScheme();
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme({keepTransitions: true});
   const theme = useMantineTheme();
+  const [isClient, setIsClient] = useState(false)
+ 
+  useEffect(() => {
+    setIsClient(true)
+  }, [])
 
   return (
-    <Switch
-      checked={colorScheme === 'dark'}
-      onChange={() => toggleColorScheme()}
-      size="lg"
-      color='gray'
-      onLabel={<IconSun color={theme.white} size="1.25rem" stroke={1.5} />}
-      offLabel={<IconMoonStars color={theme.colors.gray[6]} size="1.25rem" stroke={1.5} />}
-      className={classes.switch}
-    />
+    <>
+      {isClient ?
+        <Switch
+          checked={colorScheme === 'dark'}
+          onChange={() => toggleColorScheme()}
+          size="lg"
+          color='gray'
+          onLabel={<IconSun color={theme.white} size="1.25rem" stroke={1.5} />}
+          offLabel={<IconMoonStars color={theme.colors.gray[6]} size="1.25rem" stroke={1.5} />}
+          className={classes.switch}
+        />
+        :
+        <Switch
+          onChange={() => toggleColorScheme()}
+          size="lg"
+          color='gray'
+          onLabel={<IconSun color={theme.white} size="1.25rem" stroke={1.5} />}
+          offLabel={<IconMoonStars color={theme.colors.gray[6]} size="1.25rem" stroke={1.5} />}
+          className={classes.switch}
+        />
+      }
+    </>
   );
 }
