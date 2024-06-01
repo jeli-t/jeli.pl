a1eeb235e57204f0f5b4574140557616a6fade71
jeli-t <jelitotomek@gmail.com>
2024-06-01 17:23:48 +0200

    redesign landing page

--- frontend/app/[locale]/page.tsx
+++ frontend/app/[locale]/page.tsx
@@ -4,8 +4,6 @@ import { HeroSection } from '@/components/HeroSection/HeroSection';
 import { AboutPreview } from '@/components/AboutPreview/AboutPreview';
 import { WhatDoIDo } from '@/components/WhatDoIDo/WhatDoIDo';
 import { CallToAction } from '@/components/CallToAction/CallToAction';
-import { PortfolioPreview } from '@/components/PortfolioPreview/PortfolioPreview';
-import { ProductsPreview } from '@/components/ProductsPreview/ProductsPreview';
 import {getTranslations} from 'next-intl/server';
 
 
@@ -32,8 +30,6 @@ export default function Home() {
     <Container fluid p={0}>
       <HeroSection></HeroSection>
       <WhatDoIDo></WhatDoIDo>
-      <ProductsPreview></ProductsPreview>
-      <PortfolioPreview></PortfolioPreview>
       <AboutPreview></AboutPreview>
       <CallToAction></CallToAction>
     </Container>
--- frontend/components/WhatDoIDo/WhatDoIDo.tsx
+++ frontend/components/WhatDoIDo/WhatDoIDo.tsx
@@ -1,8 +1,12 @@
-import { Text, SimpleGrid, Container, rem, Title } from '@mantine/core';
-import { IconTie, IconPuzzle, IconUserCheck } from '@tabler/icons-react';
+import { Container, Title, Text, Button, Flex, List, ListItem, rem, ThemeIcon } from '@mantine/core';
 import classes from './WhatDoIDo.module.css';
 import {useTranslations} from 'next-intl';
-
+import {Link} from '@/navigation';
+import Image from 'next/image'
+import furniro from './furniro.png';
+import agency from './agency.png';
+import crisis_corridor from './crisis_corridor.png';
+import { IconCircleCheck } from '@tabler/icons-react';
 
 export function WhatDoIDo() {
   const t = useTranslations('WhatDoIDo');
@@ -10,44 +14,74 @@ export function WhatDoIDo() {
   return (
     <Container fluid className={classes.component}>
       <Container mt={30} mb={30} size="xl">
-        <SimpleGrid cols={{ base: 1, sm: 3 }} spacing={50}>
-          <div className={classes.feature}>
-            <div className={classes.overlay} />
-            <div className={classes.content}>
-              <IconPuzzle style={{ width: rem(38), height: rem(38) }} className={classes.icon} stroke={1.5} />
-              <Title order={2} fw={700} fz="xl" mb="xs" mt={5} className={classes.title}>
-                {t("title1")}
+        <Flex direction="column" align="center" justify="center" gap={60}>
+          <Flex direction={{ base: 'column', sm: 'row' }} align="center" justify="center">
+            <a href="https://agency.jeli.pl" target="_blank" className={classes.link}>
+              <Image src={agency} alt="agency.jeli.pl" title="agency.jeli.pl" className={classes.image} />
+            </a>
+            <Container className={classes.content}>
+              <Title order={2} className={classes.title}>
+                {t('title1')}
               </Title>
               <Text className={classes.description}>
-                {t("description1")}
+                {t('description1_1')}
+                <br />
+                {t('description1_2')}
               </Text>
-            </div>
-          </div>
-          <div className={classes.feature}>
-            <div className={classes.overlay} />
-            <div className={classes.content}>
-              <IconUserCheck style={{ width: rem(38), height: rem(38) }} className={classes.icon} stroke={1.5} />
-              <Title order={2} fw={700} fz="xl" mb="xs" mt={5} className={classes.title}>
-                {t("title2")}
+              <List mt={10} className={classes.description} center spacing="md" icon={
+                <ThemeIcon size={24} radius={"xl"}>
+                  <IconCircleCheck style={{ width: rem(18), height: rem(18) }} />
+                </ThemeIcon>}
+              >
+                <ListItem>{t('bullet1_1')}</ListItem>
+                <ListItem>{t('bullet1_2')}</ListItem>
+                <ListItem>{t('bullet1_3')}</ListItem>
+                <ListItem>{t('bullet1_4')}</ListItem>
+              </List>
+            </Container>
+          </Flex>
+          <Flex direction={{ base: 'column', sm: 'row-reverse' }} align="center" justify="center">
+            <a href="https://furniro.jeli.pl" target="_blank" className={classes.link}>
+              <Image src={furniro} alt="furniro.jeli.pl" title="furniro.jeli.pl" className={classes.image} />
+            </a>
+            <Container className={classes.content}>
+              <Title order={2} className={classes.title}>
+                {t('title2')}
               </Title>
               <Text className={classes.description}>
-                {t("description2")}
+                {t('description2_1')}
+                <br />
+                {t('description2_2')}
               </Text>
-            </div>
-          </div>
-          <div className={classes.feature}>
-            <div className={classes.overlay} />
-            <div className={classes.content}>
-              <IconTie style={{ width: rem(38), height: rem(38) }} className={classes.icon} stroke={1.5} />
-              <Title order={2} fw={700} fz="xl" mb="xs" mt={5} className={classes.title}>
-                {t("title3")}
+              <List mt={10} className={classes.description} center spacing="md" icon={
+                <ThemeIcon size={24} radius={"xl"}>
+                  <IconCircleCheck style={{ width: rem(18), height: rem(18) }} />
+                </ThemeIcon>}
+              >
+                <ListItem>{t('bullet2_1')}</ListItem>
+                <ListItem>{t('bullet2_2')}</ListItem>
+              </List>
+            </Container>
+          </Flex>
+          <Flex direction={{ base: 'column', sm: 'row' }} align="center" justify="center">
+            <a href='https://github.com/jeli-t/zegluga-game-jam-2023' target="_blank" className={classes.link}>
+              <Image src={crisis_corridor} alt="Crisis Corridor Game" title="Crisis Corridor Game" className={classes.image} />
+            </a>
+            <Container className={classes.content}>
+              <Title order={2} className={classes.title}>
+                {t('title3')}
               </Title>
-              <Text className={classes.description}>
-                {t("description3")}
+              <Text className={classes.description} maw={450}>
+                {t('description3')}
               </Text>
-            </div>
-          </div>
-        </SimpleGrid>
+            </Container>
+          </Flex>
+          <Link href="/portfolio">
+            <Button size="lg" mt={50} radius={"md"}>
+              {t('button')}
+            </Button>
+          </Link>
+        </Flex>
       </Container>
     </Container>
   );
